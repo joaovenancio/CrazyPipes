01105e7f7e894c7ef561aa9c38ae4267c973878d
Venâncio <joaovitorvenancio99@outlook.com>
2024-12-10 13:28:25 +0000

    feature: Add game states enum

---
+++ src/state/GameState.js
@@ -0,0 +1,15 @@
+//ENUM
+export class GameState {
+    static #_MENU = 0;
+    static #_WATER_FLOWING = 1;
+    static #_GAME_OVER = 2;
+    static #_WON = 3;
+    static #_size = 4;
+
+    static get MENU() { return this.#_MENU; }
+    static get WATER_FLOWING() { return this.#_WATER_FLOWING; }
+    static get GAME_OVER() { return this.#_GAME_OVER; }
+    static get WON() { return this.#_WON; }
+
+    static get size() { return this.#_size; }
+}
