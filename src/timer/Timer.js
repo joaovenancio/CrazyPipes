9f708b8d4e7a0a01916a93cda00a00b5c01658a5
Venâncio <joaovitorvenancio99@outlook.com>
2024-12-11 11:42:49 +0000

    feature: Add a stop variable

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,13 +1,17 @@
 export class Timer {
     elapsedTime = 0;
     timerInMiliseconds;
+    isActive = true;
 
     constructor(timerInMiliseconds) {
         this.elapsedTime = 0;
         this.timerInMiliseconds = timerInMiliseconds;
+        this.isActive = true;
     }
 
     tick(deltaTime) {
+        if (!this.isActive) return;
+
         this.elapsedTime += deltaTime;
 
         if (this.elapsedTime < this.timerInMiliseconds) return false;
