5a573d12f19fbe223b8cd17c00246b7c6ed2f27a
Venâncio <joaovitorvenancio99@outlook.com>
2024-12-11 10:41:39 +0000

    feature: Game over screen

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -7,13 +7,17 @@ export class GameOver extends Scene
         super('GameOver');
     }
 
+    preload() {
+        this.loadGlobalVariables();
+    }
+
     create ()
     {
         this.cameras.main.setBackgroundColor(0xff0000);
 
-        this.add.image(512, 384, 'background').setAlpha(0.5);
+        this.add.image(this.game.config.width/2, this.game.config.height/2, 'background').setAlpha(0.5);
 
-        this.add.text(512, 384, 'Game Over', {
+        this.add.text(this.game.config.width/2, this.game.config.height/2, 'Game Over', {
             fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
@@ -21,8 +25,16 @@ export class GameOver extends Scene
 
         this.input.once('pointerdown', () => {
 
+
             this.scene.start('MainMenu');
 
         });
+    }
+
+    loadGlobalVariables() {
+        
+        this.gameplayConfig = this.registry.get('gameplaySettings');
+        this.screenCenter = this.registry.get('screenCenter');
+
     }
 }
