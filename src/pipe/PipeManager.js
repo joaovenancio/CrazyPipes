25ed3717a479ce49003b95dd1ca3ce5df307f2d5
Venâncio <joaovitorvenancio99@outlook.com>
2024-12-11 23:27:05 +0000

    refact: Cleaning comments and reorganizing

--- src/pipe/PipeManager.js
+++ src/pipe/PipeManager.js
@@ -163,68 +163,54 @@ export const PIPES = {
 export class PipeManager extends Phaser.Physics.Arcade.Group {
     
     gameplayConfig = null;
-    //nextPipe = null;
     pipesOnBoard = new SinglyLinkedList ();
     conveyorPipes = new SinglyLinkedList ();
     static abra = 3;
     conveyorFirst = null;
     conveyorLast = null;
     currentPipe = null;
+    conveyorCreated = false;
+    pipeMovementOnConveyorSpeed = 600;
     
     constructor (scene, groupConfig, gameplayConfig) {
-
         super(scene.physics.world, scene, null, groupConfig);
 
         this.gameplayConfig = gameplayConfig;
-
     }
 
-    conveyorCreated = false;
 
     createPipe(pipeType, position, typeOfPipeHolder) {
-
         let newPipe = new Pipe(this.scene, position, pipeType);
         newPipe.setOrigin(0);
-
         this.add(newPipe, true);
         
         if (typeOfPipeHolder == null) return newPipe;
 
         switch  (typeOfPipeHolder) {
-
             case PipeHolder.CONVEYOR:
                 this.addToConveyor(newPipe);
                 break;
-
+                
             case PipeHolder.BOARD:
                 this.addToBoard(newPipe); //TO-DO
                 break;
-
         }
         
         return newPipe;
-
     }
-
-
-
-    
 
     getNextPipe(cells) {  //TO-DO
         let nextPipe = null;
 
-        //let currentPosition = this.currentPipe.cell.matrixPosition;
         let currentMatrixPosition = this.currentPipe.cell.matrixPosition;
         let nextPosition = [...currentMatrixPosition];
         let currentFlowDirection = this.currentPipe.waterFlowing;
-        
-        console.log(nextPosition);
-        console.log(currentFlowDirection);
 
         switch (currentFlowDirection) {
             case WaterFlowing.RIGHT:
                 nextPosition[1]++;
                 break;
+
             case WaterFlowing.LEFT:
                 nextPosition[1]--;
                 break;
@@ -238,7 +224,6 @@ export class PipeManager extends Phaser.Physics.Arcade.Group {
                 break;
         }
 
-
         if ( cells[nextPosition[0]] == null ) return null;
         let nextCell = cells[nextPosition[0]][nextPosition[1]];
 
@@ -246,116 +231,74 @@ export class PipeManager extends Phaser.Physics.Arcade.Group {
         nextPipe = nextCell.pipe;
 
         if ( nextPipe == null) return null;
-        
-        console.log('*');
-        console.log(nextPipe.isWaterAllowed);
-        console.log('*');
 
-
-        switch (currentFlowDirection) { //!!!!!
-
+        switch (currentFlowDirection) {
             case WaterFlowing.RIGHT:
-                console.log('R');
-                
                 if (nextPipe.pipeOrientation === 'straight') {
-
                     if (this.setFlowWaterRight(nextPipe)) return nextPipe;
-
                 } 
                 else if (nextPipe.pipeOrientation === 'curve') {
-
-                    console.log('curve');
-                    //if (this.setFlowWaterCurveUP(nextPipe)) return nextPipe;
-                    
                     if (!nextPipe.isWaterAllowed.left) return null;
 
                     if (nextPipe.pipeName === 'CURVE_LU') {
-                        console.log("@@@@@@@@@@@@@@@@");
-
                         nextPipe.isWaterAllowed.left = false;
                         nextPipe.isWaterAllowed.up = false;
                         nextPipe.waterFlowing = WaterFlowing.UP;
 
                         return nextPipe;
-
                     } 
                     else if (nextPipe.pipeName === 'CURVE_LD') {
-
                         nextPipe.isWaterAllowed.left = false;
                         nextPipe.isWaterAllowed.down = false;
                         nextPipe.waterFlowing = WaterFlowing.DOWN;
 
                         return nextPipe;
                     }
-
-
-                    //else if (this.setFlowWaterCurveDown(nextPipe)) return nextPipe;
-
                 } 
                 else if (nextPipe.pipeOrientation === 'cross') {
-
                     if (this.setFlowWaterRight(nextPipe)) return nextPipe;
                     else if (this.setFlowWaterUP(nextPipe)) return nextPipe;
                     else if (this.setFlowWaterDown(nextPipe)) return nextPipe;
-
                 }
             break;
 
             case WaterFlowing.LEFT:
-                console.log('L');
-
                 if (nextPipe.pipeOrientation === 'straight') {
-
                     if (this.setFlowWaterLeft(nextPipe)) return nextPipe;
-
                 } 
                 else if (nextPipe.pipeOrientation === 'curve') {
-
                     if (!nextPipe.isWaterAllowed.right) return null;
 
                     if (nextPipe.pipeName === 'CURVE_RU') {
-                        console.log("@@@@@@@@@@@@@@@@");
-
                         nextPipe.isWaterAllowed.right = false;
                         nextPipe.isWaterAllowed.up = false;
                         nextPipe.waterFlowing = WaterFlowing.UP;
 
                         return nextPipe;
-
                     } 
                     else if (nextPipe.pipeName === 'CURVE_RD') {
-
                         nextPipe.isWaterAllowed.right = false;
                         nextPipe.isWaterAllowed.down = false;
                         nextPipe.waterFlowing = WaterFlowing.DOWN;
 
                         return nextPipe;
                     }
-
                 } 
                 else if (nextPipe.pipeOrientation === 'cross') {
-
                     if (this.setFlowWaterLeft(nextPipe)) return nextPipe;
                     else if (this.setFlowWaterUP(nextPipe)) return nextPipe;
                     else if (this.setFlowWaterDown(nextPipe)) return nextPipe;
-
                 }
                 break;
 
                 case WaterFlowing.UP:
-                    console.log('U');
-
                     if (nextPipe.pipeOrientation === 'straight') {
-
                         if (this.setFlowWaterUP(nextPipe)) return nextPipe;
-
                     } 
                     else if (nextPipe.pipeOrientation === 'curve') {
-
                         if (!nextPipe.isWaterAllowed.down) return null;
 
                         if (nextPipe.pipeName === 'CURVE_LD') {
-
                             nextPipe.isWaterAllowed.left = false;
                             nextPipe.isWaterAllowed.down = false;
                             nextPipe.waterFlowing = WaterFlowing.LEFT;
@@ -363,35 +306,25 @@ export class PipeManager extends Phaser.Physics.Arcade.Group {
                             return nextPipe;
                         }
                         else if (nextPipe.pipeName === 'CURVE_RD') {
-
                             nextPipe.isWaterAllowed.right = false;
                             nextPipe.isWaterAllowed.down = false;
                             nextPipe.waterFlowing = WaterFlowing.RIGHT;
     
                             return nextPipe;
                         }
-                        
-
                     } 
                     else if (nextPipe.pipeOrientation === 'cross') {
-
                         if (this.setFlowWaterDown(nextPipe)) return nextPipe;
                         else if (this.setFlowWaterRight(nextPipe)) return nextPipe;
                         else if (this.setFlowWaterUP(nextPipe)) return nextPipe;
-
                     }
                 break;
 
             case WaterFlowing.DOWN:
-                console.log('D');
-
                 if (nextPipe.pipeOrientation === 'straight') {
-
                     if (this.setFlowWaterDown(nextPipe)) return nextPipe;
-
                 } 
                 else if (nextPipe.pipeOrientation === 'curve') {
-                    
                     if (!nextPipe.isWaterAllowed.up) return null;
                     
                     if (nextPipe.pipeName === 'CURVE_LU') {
@@ -402,114 +335,72 @@ export class PipeManager extends Phaser.Physics.Arcade.Group {
                         nextPipe.waterFlowing = WaterFlowing.LEFT;
 
                         return nextPipe;
-
                     } 
                     else if (nextPipe.pipeName === 'CURVE_RU') {
-                        console.log("RU");
-
                         nextPipe.isWaterAllowed.right = false;
                         nextPipe.isWaterAllowed.up = false;
                         nextPipe.waterFlowing = WaterFlowing.RIGHT;
 
                         return nextPipe;
-
                     } 
 
                 } 
                 else if (nextPipe.pipeOrientation === 'cross') {
-
                     if (this.setFlowWaterRight(nextPipe)) return nextPipe;
                     else if (this.setFlowWaterUP(nextPipe)) return nextPipe;
                     else if (this.setFlowWaterDown(nextPipe)) return nextPipe;
-
                 }
                 break;
-
         }
-
         
-        console.log(nextCell.pipe);
-
-
-        //this.currentPipe = nextPipe;
         return null;
-
     }
 
     setFlowWaterUP(nextPipe) {
-
         if (! nextPipe.isWaterAllowed.down ) return false;
 
         nextPipe.isWaterAllowed.down = false;
         nextPipe.isWaterAllowed.up = false;
         nextPipe.waterFlowing = WaterFlowing.UP;
-        console.log('GOOOOOD!');
-
 
         return true;
-
     }
 
     setFlowWaterCurveUP(nextPipe) {
-
-        console.log('Curved up');
-
         if ( nextPipe.isWaterAllowed.left || nextPipe.isWaterAllowed.right ) {
-            console.log("@@@@@@@@@@@@@@@@");
-
             nextPipe.isWaterAllowed.left = false;
             nextPipe.isWaterAllowed.right = false;
             nextPipe.isWaterAllowed.up = false;
             nextPipe.waterFlowing = WaterFlowing.UP;
 
-            console.log(nextPipe.waterFlowing);
-
             return true;
         } 
 
-
         return false;
-
-        
-
-
     }
     
 
     setFlowWaterLeft(nextPipe) {
-
-        console.log('___________');
-        console.log(nextPipe);
-        console.log(nextPipe.isWaterAllowed.right);
-        console.log('___________');
-
         if (!nextPipe.isWaterAllowed.right) return false;
 
         nextPipe.isWaterAllowed.right = false;
         nextPipe.isWaterAllowed.left = false;
         nextPipe.waterFlowing = WaterFlowing.LEFT;
-
-        console.log('GOOOOOD!');
         
         return true;
-
     }
 
     setFlowWaterDown (nextPipe) {
-
         if (!nextPipe.isWaterAllowed.up) return false;
 
         nextPipe.isWaterAllowed.up = false;
         nextPipe.isWaterAllowed.down = false;
         nextPipe.waterFlowing = WaterFlowing.DOWN;
-        console.log('GOOOOOD!');
 
         return true;
-
     }
 
     setFlowWaterCurveDown (nextPipe) {
-
         if (!nextPipe.isWaterAllowed.left ||
             !nextPipe.isWaterAllowed.right
         ) return false;
@@ -521,29 +412,19 @@ export class PipeManager extends Phaser.Physics.Arcade.Group {
         else nextPipe.isWaterAllowed.right = false;
 
         return true;
-
     }
 
     setFlowWaterRight (nextPipe) {
-        // console.log('___________');
-        // console.log(nextPipe);
-        // console.log(nextPipe.isWaterAllowed.left);
-        // console.log('___________');
-
         if (!nextPipe.isWaterAllowed.left) return false;
 
         nextPipe.isWaterAllowed.right = false;
         nextPipe.isWaterAllowed.left = false;
         nextPipe.waterFlowing = WaterFlowing.RIGHT;
 
-        console.log('GOOOOOD!');
-
         return true;
-
     }
 
     invertConveyorPipesList () {
-
         let pipe = null;
         const pipesListInverted = new SinglyLinkedList();
 
@@ -553,40 +434,29 @@ export class PipeManager extends Phaser.Physics.Arcade.Group {
         }
         
         this.conveyorPipes = pipesListInverted;
-
     }
 
     addToConveyor(pipe) {
-
         if (this.conveyorPipes.length >= this.gameplayConfig.board.qtyLines) {
             console.warn('PipeManager -> addToConveyor(pipe): Conveyour is full. Returning only the pipe');
             return pipe;
         }
 
         this.conveyorPipes.push(pipe);
-        
     }
 
     updatePipesInConveyor() {
-//        let pipe = this.createPipe(PIPES.SRAIGHT_LR, [0,0] , PipeHolder.CONVEYOR);
-        
         let pipes = this.conveyorPipes.toArray();
-        
         let pipe = this.conveyorPipes.last();
-        pipe.moveTo( [this.gameplayConfig.conveyor.pipePositions[pipes.length-1][0], this.gameplayConfig.conveyor.pipePositions[pipes.length-1][1]], 600);
-
-
-        //pipe.moveTo( [...this.gameplayConfig.conveyor.pipePositions[0]], 600);
+        pipe.moveTo( [this.gameplayConfig.conveyor.pipePositions[pipes.length-1][0], this.gameplayConfig.conveyor.pipePositions[pipes.length-1][1]], this.pipeMovementOnConveyorSpeed);
 
         let currentPipe = this.gameplayConfig.board.qtyLines-1;
         for (let i = 0; i < pipes.length; i++) {
             let position = this.gameplayConfig.conveyor.pipePositions[i];
             if (position == null) continue;
-            pipes[currentPipe].moveTo( [position[0], position[1]], 600);
+            pipes[currentPipe].moveTo( [position[0], position[1]], this.pipeMovementOnConveyorSpeed);
             currentPipe--;
         }
-
-       // return pipe;
     }
 
     addToBoard(pipe) {
@@ -594,17 +464,11 @@ export class PipeManager extends Phaser.Physics.Arcade.Group {
     }
 
     getFirstPipe() {
-        //let pipe = this.pipesOnConveyor.get(this.gameplayConfig.board.qtyLines-1);
         var pipeMan = this.conveyorPipes.first();
-        //console.log(this.conveyorPipes);
-
         pipeMan.moveTo([pipeMan.x,pipeMan.y + 500], 4000);
         
         this.conveyorPipes.shift(0);
 
         return pipeMan;
     }
-
-
-
 }
