d583d3a869136966c021aaa7282e99d5af0e4d10
Venâncio <joaovitorvenancio99@outlook.com>
2024-12-11 13:03:36 +0000

    refact: Cleaning some code

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -12,7 +12,6 @@ import { GameState } from '../state/GameState';
 
 export class Game extends Scene
 {
-
     gameplayConfig = null;
     conveyor = null;
     conveyorSpriteHeight = 0;
@@ -33,6 +32,7 @@ export class Game extends Scene
     scoreText = null;
     points = 0;
     secondsPassed = 0;
+    firstPipe = null;
 
 
     constructor ()
@@ -51,7 +51,6 @@ export class Game extends Scene
     }
 
     create () {   
-        
         this.setupStartingVariables();
         this.setupBackground();
         this.setupBoard();
@@ -63,47 +62,33 @@ export class Game extends Scene
         this.setupPipeQueue(); 
         this.setupAudio();
         this.setupStartingPipe();
+
         this.timer = new Timer(this.gameplayConfig.pipeFillTime);
 
         this.startAudio();
         this.play();
-
     }
     
     update(time, delta) {
-
         switch (this.gameState) {
             case GameState.WATER_FLOWING:
                 this.runWaterOnPipes(delta);
                 break;
-
             case GameState.PLAYING: 
                 this.runCountdown(delta);
                 break
-
         }
-
-        //if (this.gameState !== GameState.WATER_FLOWING) return;
-        
     }
 
+
     runWaterOnPipes(delta) {
-        if (this.timer.tick(delta)) {
-
+        if (this.timer.tick(delta)) { //True if defined time has passed
             this.secondsPassed++;
-
             this.sound.play('tick');
 
             if (this.secondsPassed > this.gameplayConfig.pipeTotalFillTime) {
-
-                //console.log(this.pipeManager);
-                let nextPipe = this.pipeManager.getNextPipe(this.board.cells);  //TO-DO !!!!!!!!!
-                console.log('!!!!!!!!!!!!!!!');
-                console.log(nextPipe);
-
+                let nextPipe = this.pipeManager.getNextPipe(this.board.cells);
                 this.pipeManager.currentPipe = nextPipe;
-
-                console.log(nextPipe);
 
                 if ( nextPipe === null) {
                     this.gameOver();
@@ -113,13 +98,11 @@ export class Game extends Scene
                 this.addPoints();
                 nextPipe.startFlow();
                 this.secondsPassed = 0;
-                //this.pipeManager.currentPipe = nextPipe;
-                return;
 
+                return;
             }
 
-            this.pipeManager.currentPipe.flow(); //pipeTotalFillTime
-
+            this.pipeManager.currentPipe.flow();
         }
     }
 
@@ -135,7 +118,6 @@ export class Game extends Scene
             this.scoreText.text = 'Score: ' + this.points;
         }
     }
-
 
     setupStartingVariables() {
         this.conveyor = null;
@@ -164,20 +146,16 @@ export class Game extends Scene
         this.backgroundBox.displayHeight = boardBounds.height + ((16 + 16) * boardScale);
     }
 
-    startAudio() {
-        //this.sound.get('musicPookatori').play();
-        this.sound.get('musicHoliznaEncounter').play();
-    }
-
     setupTexts() {
-
         let textStyle = {fontSize: '50px', fill: '#000000', fontFamily: 'Arial Black', stroke: '#FFFFFF', strokeThickness: 4, align: 'center'};
 
         this.scoreText = this.add.text(this.screenCenter[0], this.scoreTextPositionY, this.countdown, textStyle)
-        .setOrigin(0.5,0); //.setAbove();
+        .setOrigin(0.5,0);
+    }
 
-        console.log("OIIIIIIIIIII?");
-
+    startAudio() {
+        //this.sound.get('musicPookatori').play();
+        this.sound.get('musicHoliznaEncounter').play();
     }
 
     addPoints() {
@@ -188,45 +166,34 @@ export class Game extends Scene
 
     gameOver() {
         this.gameState = GameState.GAME_OVER;
-
         this.events.removeListener('cellClicked');
-
         this.sound.play('sfxWrong');
-
         this.gameplayConfig.score = this.points;
 
         console.log('Game -> GameOver(): Final score = ' + this.points);
-
         this.scene.start('GameOver');
     }
 
-
     setupStartingPipe() {
-
         let boardLines = this.gameplayConfig.board.qtyLines;
         let boardColumns = this.gameplayConfig.board.qtyColumns;
 
         let posX = Phaser.Math.Between( 0, boardLines-2);
         let posY = Phaser.Math.Between( 0, boardColumns-1);
-
         let cell = this.board.cells[posX][posY];
 
         let possiblePipes = this.getPossibleStartPipeTypes(posX, posY, boardColumns);
-
         let pipeType = possiblePipes[Phaser.Math.Between( 0, possiblePipes.length-1)]; 
 
         let startingPipe = this.pipeManager.createPipe(pipeType, cell.localPosition, PipeHolder.BOARD) ;
-
         this.board.container.add(startingPipe);
         this.pipeManager.currentPipe = startingPipe;
 
         cell.pipe = startingPipe;
         startingPipe.cell = cell;
-        
     }
 
     getPossibleStartPipeTypes(posX, posY, boardLines) {
-
         let possiblePiples = [];
 
         if (posX === 0) {
@@ -245,32 +212,20 @@ export class Game extends Scene
         }       
 
         return possiblePiples;
-
     }
 
     play () {
-
         this.gameState = GameState.PLAYING;
-
-        this.startWaterFlow();
-    }
-
-    startWaterFlow() {
-
     }
 
     initializeGlobalEvents() {
-
-        this.events.removeListener('cellClicked', this.onCellClick.bind(this));
+        this.events.removeListener('cellClicked', this.onCellClick.bind(this)); //TO-DO: Do i need it here?
         this.events.addListener('cellClicked', this.onCellClick.bind(this));
-
     }
 
     setupConveyor() {
-
         this.conveyor = new Conveyor(this, 0 , 0 , this.gameplayConfig);
         this.conveyor.render();
-        
     }
 
     getPipeQueuePosition(line) {
@@ -283,11 +238,9 @@ export class Game extends Scene
     }
 
     setupPipeQueue() {
-
         let pipe = null;
 
         for (let line = 0; line < this.gameplayConfig.board.qtyLines; line++) {
-
             let pipePosition = this.getPipeQueuePosition(line);
             this.updatePipePositionsVariable(line, pipePosition)
             
@@ -295,157 +248,114 @@ export class Game extends Scene
                 this.setupNewPipe(pipePosition, PipeHolder.CONVEYOR),
                 true
             );
-
         }
         
         this.pipeManager.invertConveyorPipesList();
-
-        this.pipeManager.nextPipe = pipe; //Do we need this?
-
+        this.pipeManager.nextPipe = pipe; //TO-DO: Do we need this?
     }
 
     setupAudio() {
-
         this.sound.add('musicHoliznaEncounter', {loop: true} );
         //this.sound.add('musicPookatori', {loop: true} );
-
     }
 
     setupPipeManager () {
-
         this.pipeManager = this.physics.add.existing(new PipeManager(this,  {
             immovable: true
             // classType : Pipe,
             // runChildUpdate : true
         }, this.gameplayConfig), true);
-
     }
 
-    setupNewPipe(position, typeOfPipeHolder, typeOfPipe) {//!!!!
-
+    setupNewPipe(position, typeOfPipeHolder, typeOfPipe) {
         let pipeConfig = typeOfPipe; 
 
         if (typeOfPipe == null) {
             switch (Phaser.Math.Between( 1, PIPES.length)) {
                 case 1:
                     pipeConfig = PIPES.SRAIGHT_LR;
-                    break;
-                
+                    break;  
                 case 2:
                     pipeConfig = PIPES.SRAIGHT_UD;
                     break;
-    
                 case 3:
                     pipeConfig = PIPES.CURVE_LD;
                     break;
-    
-    
                 case 4:
                     pipeConfig = PIPES.CURVE_LU;
-                    break;
-    
-    
+                    break;    
                 case 5:
                     pipeConfig = PIPES.CURVE_RD;
-                    break;
-    
+                    break;    
                 case 6:
                     pipeConfig = PIPES.CURVE_RU;
                     break;
             }
         }
 
-        let newPipe = this.pipeManager.createPipe(pipeConfig, position, typeOfPipeHolder);
-
-        return newPipe;
-        
+        return this.pipeManager.createPipe(pipeConfig, position, typeOfPipeHolder);
     }    
 
     setupBoard() {
-
-        this.board = new Board(this, [this.gameplayConfig.board.qtyLines, this.gameplayConfig.board.qtyColumns], 'square', 0,0, this.gameplayConfig);
-        
+        this.board = new Board(this, [this.gameplayConfig.board.qtyLines, this.gameplayConfig.board.qtyColumns], 'square', 0,0, this.gameplayConfig);      
         this.board.centralize();
 
         this.boardPositionY = this.board.container.y;
         this.boardPositionX = this.board.container.x;
-
     }
 
     loadGlobalVariables() {
-        
         this.gameplayConfig = this.registry.get('gameplaySettings');
         this.screenCenter = this.registry.get('screenCenter');
 
         this.conveyorY = this.conveyorY * this.gameplayConfig.board.scale
         this.gameState = this.gameplayConfig.state;
-
     }
 
     setupBackground() {
         this.add.image(...this.screenCenter, 'background');
-
         this.backgroundBox = this.add.image(this.screenCenter[0], this.screenCenter[1] + this.gameplayConfig.board.paddingY, 'box').setOrigin(0.5);
     }
-
-    // setupTexts() {
-    //     // this.add.text(...screenCenter, 'Main Menu', {
-    //     //     fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-    //     //     stroke: '#000000', strokeThickness: 8,
-    //     //     align: 'center'
-    //     // }).setOrigin(0.5);
-    // }
 
     setupStatusBar() {
         let statusBar = this.add.image(this.statusPositionX, this.statusPositionY, 'statusBar').setOrigin(0);
         statusBar.setScale(this.statusBarScale);
     }
 
-    updatePipePositionsVariable(line, position) { //Should be on conveyor?
-
+    updatePipePositionsVariable(line, position) { //Should this be on conveyor?
         let pipePositons = this.gameplayConfig.conveyor.pipePositions;
-
         pipePositons[line] = [...position]
-
     }
 
-    firstPipe = null;
-
     freeCellClicked(cell) {
-
         if (this.pipeManager == null) return;
         
-        this.sound.play('sfxPipePlace');
-        
+        this.sound.play('sfxPipePlace');    
         this.createNewConveyorPipe();
 
-        let pipe = this.setupNewPipe(cell.localPosition, PipeHolder.BOARD, this.firstPipe.pipeConfig); //TO-DO
+        let pipe = this.setupNewPipe(cell.localPosition, PipeHolder.BOARD, this.firstPipe.pipeConfig);
         this.board.container.add(pipe, true);
-
         cell.pipe = pipe;
         pipe.cell = cell;
         
-        this.firstPipe.destroy();
+        this.firstPipe.destroy(); //Or do some animation with it
     }
 
     createNewConveyorPipe() {
-
         this.firstPipe = this.pipeManager.getFirstPipe();
 
         let lastPipePosition = [...this.gameplayConfig.conveyor.pipePositions[0]];
         lastPipePosition[1] -= this.conveyorSpriteHeight;
         let pipe = this.setupNewPipe(lastPipePosition, PipeHolder.CONVEYOR);
 
-        this.pipeManager.updatePipesInConveyor();
-        
+        this.pipeManager.updatePipesInConveyor();   
         this.conveyor.add(pipe, true);
-        
     }
 
     occupiedCellClicked(cell) {
-
         this.sound.play('sfxPipeExplosion');
         this.points -= this.gameplayConfig.wrongPipePoints;
+
         if (this.gameState === GameState.WATER_FLOWING) this.scoreText.text = 'Score: ' + this.points;
 
         cell.pipe.cell = null;
@@ -455,20 +365,13 @@ export class Game extends Scene
 
         let pipe = this.setupNewPipe(cell.localPosition, PipeHolder.BOARD, this.firstPipe.pipeConfig);
         this.board.container.add(pipe, true);
-
         cell.pipe = pipe;
         pipe.cell = cell;
         
-        this.firstPipe.destroy();
-        
-
-        return;
+        this.firstPipe.destroy(); //Or some animtion
     }
 
-
-
     onCellClick (cell) {
-
         if (cell.pipe != null)  {
             this.occupiedCellClicked(cell);
             return;
@@ -477,36 +380,3 @@ export class Game extends Scene
         this.freeCellClicked(cell);
     }
 }
-
-
-
-
-
-
-
-// export class Game extends Scene
-// {
-//     constructor ()
-//     {
-//         super('Game');
-//     }
-
-//     create ()
-//     {
-//         this.cameras.main.setBackgroundColor(0x00ff00);
-
-//         this.add.image(512, 384, 'background').setAlpha(0.5);
-
-//         this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-//             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-//             stroke: '#000000', strokeThickness: 8,
-//             align: 'center'
-//         }).setOrigin(0.5);
-
-//         this.input.once('pointerdown', () => {
-
-//             this.scene.start('GameOver');
-
-//         });
-//     }
-// }
